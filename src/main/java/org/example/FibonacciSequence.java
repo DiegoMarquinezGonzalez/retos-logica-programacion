1de6f6191991422d3fbf07b4498d10d044a299b1
Diego Marquinez <mg.diego1988@gmail.com>
2024-08-31 20:25:58 +0200

    FibonacciSequence

---
+++ src/main/java/org/example/FibonacciSequence.java
@@ -0,0 +1,22 @@
+package org.example;
+
+public class FibonacciSequence {
+    public static void main(String[] args) {
+        /*
+         * Escribe un programa que imprima los 50 primeros números de la sucesión
+         * de Fibonacci empezando en 0.
+         * - La serie Fibonacci se compone por una sucesión de números en
+         *   la que el siguiente siempre es la suma de los dos anteriores.
+         *   0, 1, 1, 2, 3, 5, 8, 13...
+         */
+        long num1 = 0;
+        long num2 = 1;
+        for(int i = 1; i <= 50; i++){
+            System.out.println(num1);
+
+            long numFibonacci = num1 + num2;
+            num1 = num2;
+            num2 = numFibonacci;
+        }
+    }
+}
